846f384d4b7d83ba9edb1304051b6594352a84ad
BenTachie <tachiebenedict@gmail.com>
2025-09-06 20:35:31 +0200

    refactor: replace prop drilling with Context API for user data management

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserContext = React.createContext({
+  userData: {
+    name: "",
+    email: "",
+    age: "",
+    bio: "",
+  },
+});
+
+export default UserContext;
